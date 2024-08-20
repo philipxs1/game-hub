140a177e87e6c9a62654d819497b39c53535a947
Philip Schultz <philipschultz@me.com>
2024-08-20 15:03:37 -0700

    build expandable Text

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/Game";
+import { Game } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import GameScore from "./GameScore";
 import getCroppedImageUrl from "../services/image-url";
