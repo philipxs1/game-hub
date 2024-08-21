d738ee74b09125a7af517756bce11deb7c1f855c
Philip Schultz <philipschultz@me.com>
2024-08-21 00:02:18 -0700

    fixed imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App.tsx";
 import { ColorModeScript, ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import theme from "./ColorTheme.tsx";
