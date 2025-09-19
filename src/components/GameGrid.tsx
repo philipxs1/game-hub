4c05f730012458ea0f2296b2464d3054479f3f87
Philip Schultz <philipschultz@me.com>
2025-09-19 13:32:37 +0930

    update game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ import React from "react";
 
 const GameGrid = () => {
   const { data, error, isLoading, fetchNextPage, hasNextPage } = useGames();
-  const {} = inv
+
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   if (error) return <Text>{error.message}</Text>;
